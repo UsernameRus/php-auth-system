7b0916ec9dff4d178baa0a2671f32125712da750
Rust <rust.vc7@gmail.com>
2025-09-01 18:24:51 +0500

    Add database connection configuration

---
+++ db.php
@@ -0,0 +1,15 @@
+<?php
+
+// Параметры подключения к базе данных
+$host = 'localhost';
+$port = '3306';
+$dbname = 'auth_system';
+$username = 'root';
+$password = '';
+
+try {
+  $dsn = "mysql:host=$host;port=$port;dbname=$dbname;charset=utf8mb4";
+  $pdo = new PDO($dsn, $username, $password, $options);
+} catch (PDOException $e) {
+  die("Ошибка подключения к базе данных: " . $e->getMessage());
+}
